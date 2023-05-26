2e5cb0478c1d71414033b2600b9424f0f6fd66b3
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2023-05-26 13:28:04 +0900

    RAEDME修正+test追加

--- app/tests.py
+++ app/tests.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 
-url = "http://127.0.0.1:8000/jsonget"
+url = "https://earthprac-json.up.railway.app/jsonget"
 sess = requests.session()
 
 print(sess.get(url))
@@ -14,7 +14,18 @@ csrftoken = sess.cookies['csrftoken']
 headers = {'Content-type': 'application/json',  "X-CSRFToken": csrftoken}
 
 # 送信データ
-prm = {"param1": "パラメータ１", "param2": "パラメータ２"}
+prm = {
+'type': 'eew', # 緊急地震速報の場合、String型のeewという文字列
+'time': '1589131429000',
+'report': '1', # 第一報の場合、String型の1という文字列。最終報はString型のfinalという文字列
+'epicenter': '伊予灘', # 震源地
+'depth': '60km', # 震源の深さ
+'magnitude': 3.5, # 地震の規模を示すマグニチュード
+'latitude': 33.8, # ここの２行はおそらく緯度経度的な
+'longitude': 132.1,
+'intensity': '2',
+'index': 2
+}
 
 # JSON変換
 params = json.dumps(prm)
